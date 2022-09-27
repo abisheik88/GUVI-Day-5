2c4d2ec5465d8e88d929ef7b9133b4b6e1375c07
Abisheik <77277634+abisheik88@users.noreply.github.com>
2022-09-27 15:11:48 +0530

    question 1 (h) was uploaded

---
+++ qn 1 (h) day 5.js
@@ -0,0 +1,27 @@
+//Rotate an array by k times using anonymous functions 
+var rotate =function turn(arr,k){
+    var tmp=0;
+    let leng= arr.length;
+    //k= k%leng;
+    for( var i=0;i<k;i++){
+      tmp=arr.pop();
+      arr.unshift(tmp);
+    }
+    return arr;
+  }
+  console.log(rotate([1,2,3,4],3))
+
+
+  //rotate an array k times using  IIFE 
+
+  (function turn(arr,k){
+    var tmp=0;
+    let leng= arr.length;
+    //k= k%leng;
+    for( var i=0;i<k;i++){
+      tmp=arr.pop();
+      arr.unshift(tmp);
+    }
+    console.log(arr);
+  })
+  ([1,2,3,4],3)
